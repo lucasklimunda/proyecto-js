4653e52c047785401bc4b5faa298a34d15dec495
Lucas Klimunda <lucasklimunda@gmail.com>
2022-09-05 20:38:54 -0300

    Simulador interactivo

--- js/index.js
+++ js/index.js
@@ -1,11 +1,19 @@
-//Pedir un número por prompt, repetir la salida del mensaje "Hola" la cantidad de veces ingresada:
 
-let numero = prompt("Ingrese número de las veces que desea que se repita el mensaje")
+//Simulador interactivo : "Calcular tiempo de espera promedio en relación a la cantidad de turnos registrados."
 
-for (let i = 1; i <= numero; i++){
-  alert ("Hola " + i)
+function calcularTiempoEspera (espera){
+  for (let i = 1;  i <= 11; i++){
+      let tiempo = i * espera
+      let nombre = prompt("Por orden de llegada se le dará un tiempo de espera estimado por favor ingrese su nombre")
+      alert("Turno " + " " + i + " del paciente " + nombre + " tiempo de espera estimado " + tiempo + " minutos")
+      if (i> 10 && i < 12){
+      alert ("Se acabaron los turnos")
+      } 
+  }
 
 }
+
+calcularTiempoEspera (5)
 
 
 
